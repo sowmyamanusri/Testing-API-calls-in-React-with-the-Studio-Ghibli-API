f5a56af219a786976a6778b3d9d1df14adf150ca
sowmyamanusri <79841813+sowmyamanusri@users.noreply.github.com>
2022-05-05 16:32:39 +0100

    Added error handling condition

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,15 @@ import fetchGhilbliFilms from './component/fetchGhilbliFlims';
 
  function App() {
   const[ghilbliFilms,setGhilbliFilms] = useState<GhilbliFilms>();
-
+  const [ghilbliFilmsErrors,setGhilbliFilmsErrors] = useState<string>()
   useEffect((() => {
     const fetchFilms = async () =>{
-      const loadedGhilbliFilms = await fetchGhilbliFilms();
+      const loadedGhilbliFilms  = await fetchGhilbliFilms();
+      if(loadedGhilbliFilms === "Oops… something went wrong, try again 🤕" || loadedGhilbliFilms === undefined){
+        setGhilbliFilmsErrors(loadedGhilbliFilms);
+      }else{
       setGhilbliFilms(loadedGhilbliFilms);
+      }
   }
   fetchFilms();
   }),[])
@@ -19,12 +23,17 @@ import fetchGhilbliFilms from './component/fetchGhilbliFlims';
   return(
     <div className="container">
     <h1> GhilbliFilms</h1>
+    
+    {!ghilbliFilmsErrors && <div>
     <p>{ghilbliFilms?.id}</p>
    <p>{ghilbliFilms?.title}</p>
     <img className="img" src={ghilbliFilms?.image} alt ={ghilbliFilms?.title} />
     </div>
+    }
+    
+   </div>
   )
-  }
+ }
 
 
 export default App;
